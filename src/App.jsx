d15697aaffb97aec75a81b3bcb2a3228024f372a
Dare-devil-23 <sahithmummy22@gmail.com>
2022-11-05 14:58:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+import Blog from './pages/Blog';
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <div className='container'>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/blog/:id' component={Blog} />
+        </Switch>
+    </div>
+  );
+};
+
+export default App;
